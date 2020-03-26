9b03f84bbcd98c462740990b252316aef1df50b4
josemadp <josemadp40@uma.es>
2020-03-26 11:28:44 +0100

    Parte JosemaDP

--- Jarra.java
+++ Jarra.java
@@ -7,8 +7,13 @@ public class Jarra { //variables de instancia
 			throw new RuntimeException("ERROR: no se pueden crear jarras “ + "con capacidad menor o igual que cero.");
 		}
 		this.capacidad=capacidadInicial;
-	} //  Aquí vendrán después los métodos creados en la siguiente fase
-	public void metodoComunitario(){ // Comentario incluido para pruebas
-		System.out.println("Aqui escribimos todos");
+	}
+
+	public bool llena(){
+		return this.cantidad=this.capacidad;
+	}
+
+	public bool vacia(){
+		return this.cantidad=0;
 	}
 }
