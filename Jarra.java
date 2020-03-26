5da21b2b52a86d08fb0b19cf68fd03ab2524f690
Javierms15 <Javiimartin15@uma.es>
2020-03-26 11:29:43 +0100

    Parte Javierms15

--- Jarra.java
+++ Jarra.java
@@ -8,7 +8,18 @@ public class Jarra { //variables de instancia
 		}
 		this.capacidad=capacidadInicial;
 	} //  Aquí vendrán después los métodos creados en la siguiente fase
+	public void llenaDesde(Jarra j){
+		while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
+		this.cantidad++;
+		j.cantidad--;
+		}
+		}
+		public String toString(){
+		return ("("+this.capacidad+","+this.cantidad+")");
+		}
 	public void metodoComunitario(){ // Comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
 	}
+	
+
 }
