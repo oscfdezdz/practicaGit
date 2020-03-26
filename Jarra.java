5a64a941561a021cdf7b5830bfa288f24ab282df
oscfdezdz <oscfdezdz@tuta.io>
2020-03-26 10:03:14 +0000

    Update Jarra.java

--- Jarra.java
+++ Jarra.java
@@ -1,11 +1,13 @@
 public class Jarra { //variables de instancia
 	private int cantidad;
 	private int capacidad;
+
 	public Jarra(int capacidadInicial){
 		if(capacidadInicial<=0){
 			throw new RuntimeException("ERROR: no se pueden crear jarras “ + "con capacidad menor o igual que cero.");
-			}
-		this.capacidad=capacidadInicial;}//  Aquí vendrán después los métodos creados en la siguiente fase
+		}
+		this.capacidad=capacidadInicial;
+	} //  Aquí vendrán después los métodos creados en la siguiente fase
 	public void metodoComunitario(){ // Comentario incluido para pruebas
 		System.out.println("Aqui escribimos todos");
 	}
